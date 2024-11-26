6ad9d9a1d45b7123e7df92af8b61a9f6d673765f
vinodreddyb86 <vinod@hexacluster.ai>
2024-11-26 22:50:26 +0530

    graceful shutdown grpc server

--- greet/server/main.go
+++ greet/server/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
-	"log"
+	"context"
+	"fmt"
+	"log/slog"
 	"net"
+	"os"
+	"os/signal"
 
 	pb "github.com/vinodreddyb/go-grpc/greet/proto"
+	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
 )
 
@@ -15,19 +20,44 @@ type Server struct {
 }
 
 func main() {
-	lis, err := net.Listen("tcp", add)
 
-	if err != nil {
-		log.Fatalf("Failed to listen: %v", err)
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	defer cancel()
+
+	if err := run(ctx); err != nil {
+		slog.Error("Failed to create signal context: %v", err)
+		os.Exit(1)
 	}
 
-	s := grpc.NewServer()
+}
 
+func run(ctx context.Context) error {
+
+	g, ctx := errgroup.WithContext(ctx)
+	s := grpc.NewServer()
 	//Register the server
 	pb.RegisterGreetServiceServer(s, &Server{})
 
-	if err = s.Serve(lis); err != nil {
-		log.Fatalf("Failed to serve: %v", err)
-	}
+	g.Go(func() error {
+		lis, err := net.Listen("tcp", add)
 
+		if err != nil {
+			//log.Fatalf("Failed to listen: %v", err)
+			return fmt.Errorf("failed to listen %q : %v", add, err)
+		}
+		slog.Info("starting grpc server on address", slog.String("address", add))
+		if err = s.Serve(lis); err != nil {
+			return fmt.Errorf("failed to serve: %v", err)
+		}
+		return nil
+	})
+
+	g.Go(func() error {
+		<-ctx.Done()
+		slog.Info("Shutting down the server")
+		s.GracefulStop()
+		return nil
+	})
+
+	return g.Wait()
 }
